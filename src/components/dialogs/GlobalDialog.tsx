bbdad10d17bfa24ceb9f44c00d913af67484d0f7
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-02-18 14:18:49 +0530

    added memo in global dialog for performance

---
+++ src/components/dialogs/GlobalDialog.tsx
@@ -0,0 +1,148 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Grow from "@mui/material/Grow";
+import { TransitionProps } from "@mui/material/transitions";
+import useThemeStore from "../../services/store/theme/themeStore";
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  backdropFilter: "blur(7px)",
+  "& .MuiPaper-root": {
+    borderRadius: theme.shape.borderRadius * 3, // Adjust the multiplier to get the desired border radius
+  },
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & { children: React.ReactElement<unknown, never> },
+  ref: React.Ref<unknown>
+) {
+  return <Grow ref={ref} {...props} timeout={500} />;
+});
+
+type ButtonColor =
+  | "inherit"
+  | "primary"
+  | "secondary"
+  | "error"
+  | "info"
+  | "success"
+  | "warning";
+interface IProps {
+  open: boolean;
+  // setOpen: BooleanSetStateAction;
+  dialogActionFunction: () => void;
+  children: React.ReactNode;
+  dialogTitle: string;
+  handleClose: () => void;
+  actionButtonTitle?: string;
+  actionButtonColor?: ButtonColor;
+  isDisable?: boolean;
+  customSize?: string;
+}
+
+const GlobalDialog = React.memo(
+  ({
+    open,
+    // setOpen,
+    dialogActionFunction,
+    dialogTitle,
+    children,
+    actionButtonTitle = "Submit",
+    actionButtonColor = "primary",
+    isDisable,
+    customSize,
+    handleClose,
+  }: IProps) => {
+    const { darkMode } = useThemeStore();
+
+    const sizeModifier = () => {
+      if (customSize) {
+        if (customSize.toLowerCase() === "large") {
+          return "lg";
+        }
+      }
+      if (customSize) {
+        if (customSize.toLowerCase() === "small") {
+          return "sm";
+        }
+      }
+
+      return "md";
+    };
+
+    return (
+      <React.Fragment>
+        <BootstrapDialog
+          aria-labelledby="customized-dialog-title"
+          open={open}
+          slots={{ transition: Transition }}
+          maxWidth={sizeModifier()}
+          fullWidth
+        >
+          <DialogTitle
+            sx={{ m: 0, p: 2, background: darkMode ? undefined : undefined }}
+            id="customized-dialog-title"
+          >
+            {dialogTitle}
+          </DialogTitle>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+
+          <DialogContent
+            sx={{ background: darkMode ? undefined : undefined, minWidth: 500 }}
+            dividers
+          >
+            {children}
+          </DialogContent>
+
+          <DialogActions sx={{ background: darkMode ? undefined : undefined }}>
+            {!isDisable && (
+              <Button
+                variant="contained"
+                disabled={isDisable}
+                disableElevation
+                color={actionButtonColor}
+                onClick={() => dialogActionFunction()}
+              >
+                {actionButtonTitle}
+              </Button>
+            )}
+
+            <Button
+              variant="outlined"
+              color="secondary"
+              disableElevation
+              onClick={handleClose}
+            >
+              {"Close"}
+            </Button>
+          </DialogActions>
+        </BootstrapDialog>
+      </React.Fragment>
+    );
+  }
+);
+
+export default GlobalDialog;
